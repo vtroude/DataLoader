54b070ae1ff117e96cb8eee913f15372ad4cfb59
vtroude <virgiletroude@gmail.com>
2024-03-21 03:55:27 -0700

    push-trading-idea

--- dataloader.py
+++ dataloader.py
@@ -1,3 +1,4 @@
+import time
 import requests
 
 import pandas as pd
@@ -5,14 +6,16 @@ import pandas as pd
 from typing     import Union
 from datetime   import datetime
 
+from requests.exceptions    import ConnectionError
 
+url_base    = 'http://44.221.198.180:8000/'
+#url_base    = "http://127.0.0.1:8000/"
 
-#url_base    = 'http://44.221.198.180:8000/'
-url_base    = "http://127.0.0.1:8000/"
+API_TOKEN   = None
 
 #######################################################################################################################
 """
-
+Private Function
 """
 #######################################################################################################################
 
@@ -48,6 +51,66 @@ def init_get(url, code = None, start_time = None, end_time = None):
     url = get_url(url, code, start_time, end_time)
 
     return requests.get(url)
+
+def make_request_with_retry(url, data, headers, max_retries=5, sleep_time=5):
+    """
+    Attempts to send a POST request to the specified URL with the given data and headers.
+    Retries on ConnectionError with exponential backoff.
+    
+    :param url: URL to send the POST request to (str).
+    :param data: Data to be sent in the request (DataFrame).
+    :param headers: Request headers (dict / json).
+    :param max_retries: Maximum number of retries (int).
+    :param backoff_factor: Factor to calculate delay between retries (exponential backoff) (float).
+    """
+    if max_retries > 0:
+        datajson    = data.to_dict(orient='records')
+        try:
+            response = requests.post(url, json=datajson, headers=headers)
+
+            print(response.status_code, response.reason)
+            response.raise_for_status()  # Raises an HTTPError if the response status code is 4xx or 5xx
+
+            return []
+       
+        except ConnectionError as e:
+            print(f"ConnectionError: {e}")
+            print(f"Retrying in {sleep_time} seconds...")
+            time.sleep(sleep_time)
+            N   = len(data) // 2
+            print(len(data), N)
+            failed_1    = make_request_with_retry(url, data.iloc[:N], headers, max_retries=max_retries-1, sleep_time=sleep_time)
+            failed_2    = make_request_with_retry(url, data.iloc[N:], headers, max_retries=max_retries-1, sleep_time=sleep_time)
+        
+            return failed_1 + failed_2
+
+        except requests.exceptions.HTTPError as e:
+            # No retry for HTTP errors other than connection issues
+            print(f"HTTPError: {e.response.status_code} - {e.response.text}")
+            
+            return [data]
+
+    return [data]
+
+#######################################################################################################################
+"""
+Client methods
+"""
+#######################################################################################################################
+
+def login(username, password):
+    global API_TOKEN
+    
+    credentials = {'username': username, 'password': password}
+    token_url   = url_base + 'api/token/'
+
+    response    = requests.post(token_url, data=credentials)
+    
+    if response.status_code == 200:
+        API_TOKEN   = response.json().get('access')  # Adjust based on your API's response
+        print("Login successful")
+    else:
+        print("Login failed")
 
 
 #######################################################################################################################
@@ -135,7 +198,7 @@ def get_trading_idea(code = None, start_time = None, end_time = None):
     # Return status code if there is an error
     return response.status_code
 
-def upload_trading_idea(data: pd.DataFrame):
+def upload_trading_idea(data: pd.DataFrame, chunksize=1000):
     """
     POST method to push trading ideas to the data base
     Inputs: - data  - pandas DataFrame composed of                                                                                          (pandas.DataFrame)
@@ -166,13 +229,29 @@ def upload_trading_idea(data: pd.DataFrame):
             assert c in data.columns, f"{c} should ba column in the DataFrame"
 
     # Convert timestamp to UTC
-    try:
-        data['date']   = pd.to_datetime(data['date']).dt.tz_localize('UTC').dt.strftime("%Y-%m-%d %H:%M:%S")    # + "TZ"
-    except:
-        pass
-    print(data)
-    # Extract Json data from DataFrame
-    data    = data.to_dict(orient='records')
+    #try:
+    #    data['date']   = pd.to_datetime(data['date']).dt.tz_localize('UTC').dt.strftime("%Y-%m-%d %H:%M:%S")    # + "TZ"
+    #except:
+    #    pass
 
-    # POST the data
-    return requests.post(f"{url_base}trading-idea/upload/", json=data, headers={'Content-Type': 'application/json'})
+    url         = f"{url_base}trading-idea/upload/"
+    headers     = {'Content-Type': 'application/json'}
+    response    = None
+    failed      = []
+    for start in range(0, len(data), chunksize):
+        end = min(start + chunksize, len(data))
+        # Extract Json data from DataFrame
+        if end != start:
+            datajson    = data.iloc[start:end].to_dict(orient='records')
+
+            # POST the data
+            f   = make_request_with_retry(url, data.iloc[start:end], headers)
+            #response = requests.post(url, json=datajson, headers={'Content-Type': 'application/json'})
+
+            failed  += f
+
+    if len(failed) > 0:
+        failed  = pd.concat(failed, axis=0, ignore_index=True)
+
+    # POST the data we failed to push on the database
+    return failed
